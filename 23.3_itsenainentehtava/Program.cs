a5591d7279eddf67adb80dee2e94a18f9b8caf2e
Tanzelmi <tanja.myllynen92@gmail.com>
2023-03-26 07:56:52 +0300

    first commit

---
+++ 23.3_itsenainentehtava/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _23._3_itsenainentehtava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pyydä käyttäjältä leveys ja korkeus (metreinä).
+
+            // Ilmoita käyttäjälle hänen syöttämien tietojen perusteella pinta - ala(neliöinä).
+
+            // Laskukaava: leveys* korkeus 
+
+            Console.WriteLine("Anna leveys metreinä. ");
+            string userWidth = Console.ReadLine();
+            
+            int width = int.Parse(userWidth);
+
+            Console.WriteLine("Anna pituus metreinä. ");
+            string userLength = Console.ReadLine();
+
+            int lenght = int.Parse(userLength);
+
+            int acreage = width * lenght;
+
+            Console.WriteLine($"Annettujen mittojen pinta-ala on {acreage} neliötä. ");
+
+
+        }
+    }
+}
